e880eeb5f4fd42fc634ccfb4f9787369eab28d67
dhrubo334 <barmandhrubo565@gmail.com>
2025-07-22 11:26:32 +0530

    Add files via upload

--- day10/script.js
+++ day10/script.js
@@ -1,78 +1,26 @@
-/** @type {HTMLInputElement} */
-const display = document.getElementById("display");
+let currentIndex = 0;
 
-const signOperators = ["+", "-"];
-const otherOperators = ["*", "/"];
-const arithmeticOperators = [...signOperators, ...otherOperators];
+const images = document.querySelectorAll(".image-container img");
+const totalImages = images.length;
 
-const clearExp = () => (display.value = "0");
-const back = () => {
-  if (display.value.length > 1) {
-    display.value = display.value.slice(0, display.value.length - 1);
-  } else clearExp();
-};
+function showImage(index) {
+  images.forEach((img, i) => {
+    img.style.display = i === index ? "block" : "none";
+  });
+}
 
-const addToExp = (/** @type {string | number} */ val) => {
-  if (val === "." && !canUsePoint(display.value)) return;
+function Slide(direction) {
+  currentIndex += direction;
 
-  const lastVal = display.value[display.value.length - 1];
-  // console.log(lastVal);
-
-  if (isNaN(parseInt(lastVal))) {
-    if (arithmeticOperators.some((opt) => opt === lastVal)) {
-      if (signOperators.some((opt) => opt === lastVal)) {
-        if (signOperators.some((opt) => opt === val)) {
-          display.value =
-            display.value.slice(0, display.value.length - 1) + val;
-        } else if (otherOperators.some((opt) => opt === val)) return;
-        else display.value += val;
-      } else if (otherOperators.some((opt) => opt === lastVal)) {
-        if (otherOperators.some((opt) => opt === val)) {
-          display.value =
-            display.value.slice(0, display.value.length - 1) + val;
-        } // else if (signOperators.some((opt) => opt === val)) return;
-        else display.value += val;
-      }
-      return;
-    }
-
-    if (lastVal === "." && arithmeticOperators.some((opt) => opt === val)) {
-      return;
-    }
+  if (currentIndex >= totalImages) {
+    currentIndex = 0;
+  } else if (currentIndex < 0) {
+    currentIndex = totalImages - 1;
   }
 
-  if (display.value === "0") {
-    if (val === "00") return;
-    if (otherOperators.some((opt) => opt === val)) return;
-    display.value = val;
-  } else if (val === "%") {
-    if (!isNaN(parseInt(lastVal))) {
-      const tvalue = eval(display.value);
-      display.value = tvalue * 0.01;
-    }
-  } else display.value += val;
-};
+  showImage(currentIndex);
+}
 
-const showResult = () => {
-  display.value = eval(display.value);
-};
-
-const canUsePoint = (/** @type {string} */ str) => {
-  if (!str.length) return true;
-
-  let i = str.length - 1;
-  for (; i; i--) {
-    if (str[i] === ".") return false;
-    else if (arithmeticOperators.some((opt) => opt === str[i])) {
-      i++;
-      break;
-    }
-  }
-
-  const lastNumber = str.slice(i, str.length);
-  console.log(lastNumber);
-
-  if (!lastNumber) return true;
-  if (!isNaN(parseInt(lastNumber))) return true;
-  return false;
-};
+document.addEventListener("DOMContentLoaded", () => {
+  showImage(currentIndex);
+});
