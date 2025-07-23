37054e25e0477dff571c834fbf55422e453a088c
dhrubo334 <barmandhrubo565@gmail.com>
2025-07-23 14:12:52 +0530

    Delete day12 directory

--- day12/src/components/banner/banner.jsx
+++
@@ -1,8 +0,0 @@
-import './banner.css'
-export default function banner() {
-    return (
-        <div className="banner">
-            <img src="./banner.jpg" alt="banner" />
-        </div>
-    )
-}
--- day12/src/components/footer/footer.jsx
+++
@@ -1,8 +0,0 @@
-import "./footer.css"
-export default function footer() {
-    return (
-        <div className="footer">
-            <p>&copy; 2025 Copyright</p>
-        </div>
-    )
-}
